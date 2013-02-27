d2b8d4b13eec714380a5cf9901966eba98c66fd5
Vladimir Zaytsev <zaytsev@usc.edu>
2013-02-26 21:28:19 -0800

    implemented proof-of-concept version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Tsunami
+
+## 1. Corpora
+
+### Spanish
+
+1. [European Parliament Proceedings Parallel Corpus 1996-2011](http://www.statmt.org/europarl/)
+1. [Ancora](http://clic.ub.edu/corpus/en/ancora-descarregues)
+
+### Russian
+
+1. [Open Corpora](http://opencorpora.org/)
+1. [English-Russian temporally aligned corpus](http://cogcomp.cs.illinois.edu/page/resources/data)
+
+### Farsi
+
+1. [Hamshahri Collection V2](http://ece.ut.ac.ir/dbrg/Hamshahri/download.html#version2)
+
+### Eglish
--- tsunami.py
+++ tsunami.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Tsunami – a high performance triples extractor.
-#
 # Copyright (C) USC Information Sciences Institute
 # Author: Vladimir M. Zaytsev <zaytsev@usc.edu>
 # URL: <http://nlg.isi.edu/>
 # For more information, see README.md
 # For license information, see LICENSE
+
+import sys
+
+from extractors import MetaphorLF_Reader, TripleExtractor, TripleFold
+from triples import SubjectTriplePattern
+
+
+if __name__ == "__main__":
+
+
+    reader = MetaphorLF_Reader("parsed.txt")
+
+    ex = TripleExtractor(triple_patterns=[
+        SubjectTriplePattern(),
+    ])
+
+    i_sents = reader.i_sentences()
+
+    i_triple_sets = ex.i_extract_triples(i_sents)
+
+    tfold = TripleFold()
+
+    for t_set in i_triple_sets:
+        tfold.add_triples(t_set)
+
+    result = tfold.i_triples()
+
+    for triple_info in result:
+        sys.stdout.write(", ".join([unicode(tf).encode("utf-8")
+                                    for tf in triple_info]))
+        sys.stdout.write("\n")
