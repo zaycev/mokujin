287c21b78e61d7b044b6b93ad09e1f5c5c96e532
Vladimir Zaytsev <vladimir@zvm.me>
2013-05-15 22:15:33 -0700

    update text index

--- createlfindex.py
+++ createlfindex.py
@@ -7,4 +7,47 @@
 # For more information, see README.md
 # For license information, see LICENSE
 
+import glob
+import logging
+import argparse
+import cPickle as pickle
 
+from mokujin.logicalform import POS
+from mokujin.index import SimpleObjectIndex
+from mokujin.logicalform import MetaphorAdpLF_Reader
+
+
+def sent_to_terms(sent):
+    for p in sent:
+        if p.pos.pos != POS.NONE:
+            yield p.lemma.encode("utf-8")
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", default=None, type=str)
+    parser.add_argument("-o", "--outdir", default="text-index", type=str)
+    args = parser.parse_args()
+
+    i_files = glob.glob(args.input)
+    o_dir = args.outdir
+
+    logging.info("INPUT FILE: %r" % i_files)
+    logging.info("OUT DIR: %r" % o_dir)
+
+    obj_to_terms = sent_to_terms
+    obj_to_str = pickle.dumps
+    str_to_obj = pickle.loads
+
+    index = SimpleObjectIndex(o_dir, obj_to_terms, obj_to_str, str_to_obj)
+    index.load_all()
+    
+    for fl in i_files:
+        i_file = open(fl, "r")
+        reader = MetaphorAdpLF_Reader(i_file)
+        i_sents = reader.i_sentences()
+        index.update_index(i_sents)
+
+    logging.info("DONE")
---
+++ lfsearch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (C) USC Information Sciences Institute
+# Author: Vladimir M. Zaytsev <zaytsev@usc.edu>
+# URL: <http://nlg.isi.edu/>
+# For more information, see README.md
+# For license information, see LICENSE
+
+
+import sys
+import logging
+import argparse
+import cPickle as pickle
+
+from mokujin.logicalform import POS
+from mokujin.index import SimpleObjectIndex
+from mokujin.logicalform import MetaphorAdpLF_Reader
+from createlfindex import sent_to_terms
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--inputdir", default="lfindex", type=str)
+    parser.add_argument("-o", "--output", default=None, type=str)
+    parser.add_argument("-q", "--query", default=None, type=str)
+    args = parser.parse_args()
+
+    i_dir = args.inputdir
+    o_file = open(args.output, "w") if args.output is not None else sys.stdout
+    query_term = args.query
+    
+    logging.info("INPUT DIR: %r" % i_dir)
+    logging.info("OUT FILE: %r" % o_file)
+    logging.info("QUERY: %s" % query_term)
+
+    obj_to_terms = sent_to_terms
+    obj_to_str = pickle.dumps
+    str_to_obj = pickle.loads
+
+    index = SimpleObjectIndex(i_dir, obj_to_terms, obj_to_str, str_to_obj)
+    index.load_all()
+    results = index.find(query_term=query_term)
+
+    o_file.write("FOUND (%d):\n" % len(results))
+    for sent in results:
+        o_file.write(sent.raw_text.encode("utf-8"))
+        o_file.write("\n")
