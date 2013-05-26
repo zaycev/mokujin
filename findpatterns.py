60ba41f7213c89b9b2a09f832fcb2d17b3798925
Vladimir Zaytsev <vladimir@zvm.me>
2013-05-25 18:05:04 -0700

    add parameter to specify the file with input terms

--- findpatterns.py
+++ findpatterns.py
@@ -38,6 +38,7 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--outputdir", default="output",
                         help="Directory where script's ouput will be placed",  type=str)
     parser.add_argument("-q", "--queryterm", default=None, help="Query term", type=str)
+    parser.add_argument("-qf", "--queryterms_file", default=None, help="File with query terms", type=str)
     parser.add_argument("-s", "--stoplist", default="resources/word.freq.ru.csv", help="Stop list file", type=str)
     parser.add_argument("-ts", "--t_stop", default=500, help="Stop words frequency threshold", type=float)
     parser.add_argument("-tt", "--t_triple", default=5, help="Min frequency treshold for target triples", type=float)
@@ -60,7 +61,10 @@ if __name__ == "__main__":
 
     logging.info("INDEX DIR: %s" % args.index)
     logging.info("OUTPUT DIR: %s" % args.outputdir)
-    logging.info("QUERY TERM: %s" % args.queryterm)
+    if args.queryterm is not None:
+        logging.info("QUERY TERM: %s" % args.queryterm)
+    if args.queryterms_file is not None:
+        logging.info("QUERY TERMS FILE: %s" % args.queryterms_file)
     logging.info("NORMALIZE: %d" % args.normalize)
     logging.info("STOP LIST: %s" % args.stoplist)
     logging.info("STOP WORDS FREQ THRESHOLD: %f" % args.t_stop)
@@ -86,48 +90,61 @@ if __name__ == "__main__":
 
     explorer = TripleStoreExplorer(engine, stop_terms=stop_list, concept_net=concept_net)
     
-    term_id = explorer.engine.term_id_map.get(args.queryterm)
+    input_terms = []
+    if args.queryterm is not None:
+        input_terms.append(args.queryterm)
+    if args.queryterms_file is not None:
+        terms = open(args.queryterms_file, "rb").read().split("\n")
+        input_terms.extend(filter(lambda term: len(term) > 1, terms))
     
-    if term_id is None:
-        logging.info("QUERY TERM NOT FOUND")
-        exit(1)
+    for i, term in enumerate(input_terms):
+        print
+
+        logging.info("HANDLING %d/%d %s" % ((i + 1), len(input_terms), term))
+        term_id = explorer.engine.term_id_map.get(term)
+    
+        if term_id is None:
+            logging.info("QUERY TERM NOT FOUND")
+            exit(1)
         
-    term_triples = explorer.engine.search(arg_query=(term_id,))
+        term_triples = explorer.engine.search(arg_query=(term_id,))
     
-    pattern_collection = PatternCollection(term_id, term_triples)
-    pattern_collection.do_filter(stop_list)
+        pattern_collection = PatternCollection(term_id, term_triples)
+        pattern_collection.do_filter(stop_list)
 
-    if args.normalize == 1:
-        pattern_collection.do_norm_freq(engine)
-        pattern_collection.sort(key=lambda pattern: -pattern.norm_freq)
-    else: 
-        pattern_collection.sort(key=lambda pattern: -pattern.freq)
+        if args.normalize == 1:
+            pattern_collection.do_norm_freq(engine)
+            pattern_collection.sort(key=lambda pattern: -pattern.norm_freq)
+        else: 
+            pattern_collection.sort(key=lambda pattern: -pattern.freq)
     
-    output_name = transliterate_ru(args.queryterm)
+        output_name = transliterate_ru(term)
 
     
-    if args.debug == 1:
-        debug_fl_path = "%s/%s.debug.txt" % (args.outputdir, output_name)
-        logging.info("WRITING DEBUG TO %s" % debug_fl_path)
-        debug_fl = open(debug_fl_path, "wb")
-        pattern_collection.debug_output(debug_fl, engine)
-        debug_fl.close()
+        if args.debug == 1:
+            debug_fl_path = "%s/%s.debug.txt" % (args.outputdir, output_name)
+            logging.info("WRITING DEBUG TO %s" % debug_fl_path)
+            debug_fl = open(debug_fl_path, "wb")
+            pattern_collection.debug_output(debug_fl, engine)
+            debug_fl.close()
         
-    matrix_fl_path = "%s/%s.matrix.txt" % (args.outputdir, output_name)
-    patterns_fl_path = "%s/%s.patterns.txt" % (args.outputdir, output_name)
-    terms_fl_path = "%s/%s.terms.txt" % (args.outputdir, output_name)
-    logging.info("WRITING MATRIX TO %s" % matrix_fl_path)
-    logging.info("WRITING PATTERNS TO %s" % patterns_fl_path)
-    logging.info("WRITING TERMS TO %s" % terms_fl_path)
+        matrix_fl_path = "%s/%s.matrix.txt" % (args.outputdir, output_name)
+        patterns_fl_path = "%s/%s.patterns.txt" % (args.outputdir, output_name)
+        terms_fl_path = "%s/%s.terms.txt" % (args.outputdir, output_name)
+        logging.info("WRITING MATRIX TO %s" % matrix_fl_path)
+        logging.info("WRITING PATTERNS TO %s" % patterns_fl_path)
+        logging.info("WRITING TERMS TO %s" % terms_fl_path)
 
-    matrix_fl = open(matrix_fl_path, "wb")
-    patterns_fl = open(patterns_fl_path, "wb")
-    terms_fl = open(terms_fl_path, "wb")
+        matrix_fl = open(matrix_fl_path, "wb")
+        patterns_fl = open(patterns_fl_path, "wb")
+        terms_fl = open(terms_fl_path, "wb")
     
     
-    pattern_collection.output_matrix(engine, matrix_fl, patterns_fl, terms_fl)
+        pattern_collection.output_matrix(engine, matrix_fl, patterns_fl, terms_fl)
     
-    matrix_fl.close()
-    patterns_fl.close()
-    terms_fl.close()
+        matrix_fl.close()
+        patterns_fl.close()
+        terms_fl.close()
+
+    print
     logging.info("DONE")
