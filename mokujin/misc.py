8841fe9e8cceaaba4d5b3eb260e20fe43bd795ae
Vladimir Zaytsev <zaytsev@usc.edu>
2013-07-25 22:55:12 -0700

    Replaced custom transliteration to unidecode library

--- mokujin/misc.py
+++ mokujin/misc.py
@@ -7,51 +7,58 @@
 # For more information, see README.md
 # For license information, see LICENSE
 
-RU_EN_MAP = {
-    u"а": "a",
-    u"б": "b",
-    u"в": "v",
-    u"г": "g",
-    u"д": "d",
-    u"е": "e",
-    u"ё": "yo",
-    u"ж": "j",
-    u"з": "z",
-    u"и": "i",
-    u"й": "y",
-    u"к": "k",
-    u"л": "l",
-    u"м": "m",
-    u"н": "n",
-    u"о": "o",
-    u"п": "p",
-    u"р": "r",
-    u"с": "s",
-    u"т": "t",
-    u"у": "u",
-    u"ф": "f",
-    u"х": "h",
-    u"ц": "ts",
-    u"ч": "ch",
-    u"ш": "sh",
-    u"щ": "sch",
-    u"ъ": "",
-    u"ы": "y",
-    u"ь": "",
-    u"э": "e",
-    u"ю": "yu",
-    u"я": "ya",
-}
+import unidecode
 
+# RU_EN_MAP = {
+#     u"а": "a",
+#     u"б": "b",
+#     u"в": "v",
+#     u"г": "g",
+#     u"д": "d",
+#     u"е": "e",
+#     u"ё": "yo",
+#     u"ж": "j",
+#     u"з": "z",
+#     u"и": "i",
+#     u"й": "y",
+#     u"к": "k",
+#     u"л": "l",
+#     u"м": "m",
+#     u"н": "n",
+#     u"о": "o",
+#     u"п": "p",
+#     u"р": "r",
+#     u"с": "s",
+#     u"т": "t",
+#     u"у": "u",
+#     u"ф": "f",
+#     u"х": "h",
+#     u"ц": "ts",
+#     u"ч": "ch",
+#     u"ш": "sh",
+#     u"щ": "sch",
+#     u"ъ": "",
+#     u"ы": "y",
+#     u"ь": "",
+#     u"э": "e",
+#     u"ю": "yu",
+#     u"я": "ya",
+# }
+# 
+# 
+# def transliterate_ru(string):
+#     transliterated = ""
+#     string = string.lower()
+#     if not isinstance(string, unicode):
+#         string = string.decode("utf-8")
+#     for c in string:
+#         if c in RU_EN_MAP:
+#             transliterated += RU_EN_MAP[c]
+#         else:
+#             transliterated += c
+#     return transliterated
 
 def transliterate_ru(string):
-    transliterated = ""
-    string = string.lower()
-    if not isinstance(string, unicode):
-        string = string.decode("utf-8")
-    for c in string:
-        if c in RU_EN_MAP:
-            transliterated += RU_EN_MAP[c]
-        else:
-            transliterated += c
-    return transliterated
+    if isinstance(string, unicode):
+        return unidecode.unidecode(string)
+    return unidecode.unidecode(unicode(string))
